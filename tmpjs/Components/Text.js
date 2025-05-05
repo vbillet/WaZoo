cfc48873953ffc3cbc5dfa720840f40138eca2ce
BILLET Vincent <vbillet@gmail.com>
2025-05-05 11:27:51 +0200

    First commit for Text Component #4

---
+++ tmpjs/Components/Text.js
@@ -0,0 +1,38 @@
+class Text extends Component {
+    fontFile = ""
+    fontFamily = "Arial"
+    fontSize = 14
+    fontStyle = "normal"
+    fontVariant = "normal"
+    fontWeight = 400
+    lineHeight = 14
+
+    constructor(data=undefined) { super(data); if (data != undefined) { this.load(data) } }
+
+    load(data) {
+        super.load(data)
+    }
+
+    get text(){
+        return this._text
+    }
+
+    set text(txt){
+        this._text = txt
+        this.getSimObject().getDomElement().innerHTML = txt
+    }
+
+    getCSS(){ 
+        result = ""
+        result = result + "font-family:" + this.fontFamily + ";"
+        result = result + "font-size:" + this.fontSize + "px;"
+        result = result + "font-style:" + this.fontStyle + ";"
+        result = result + "font-variant:" + this.fontVariant + ";"
+        result = result + "font-weight:" + this.fontWeight + ";"
+        result = result + "line-height:" + this.lineHeight + "px;"
+        if (this.fontFile !=""){
+            result = "@font-face { " + result + "src:url(\"" + this.fontFile+ "\"); }"
+        }
+        return result
+    }
+}
